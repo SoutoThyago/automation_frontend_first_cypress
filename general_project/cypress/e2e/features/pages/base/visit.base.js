302825502f9a3fbd99a64904b34b7f10327cb30a
SoutoThyago <thyago.souto.ext@infracommerce.com.br>
2024-05-23 18:45:44 -0300

    add acreenshots

--- general_project/cypress/e2e/features/pages/base/visit.base.js
+++ general_project/cypress/e2e/features/pages/base/visit.base.js
@@ -4,17 +4,20 @@ require('cypress-real-events');
 export default class VisitBase {
     clickElement(element) {
         element().click()
+        cy.screenshot('click ' + element)
     }
 
     sendKey(element, text) {
         element().type(text)
+        cy.screenshot('send ' + element)
     }
 
     mouseOver(element) {        
         cy.contains("a", element).realHover('mouse')
+        cy.screenshot('houver ' + element)
     }
 
-    visible(element) {
-        return element().should('to.be.visible')      
+    visible(element) {        
+        return element().should('to.be.visible').screenshot('visible ' + element)
     }
 }
